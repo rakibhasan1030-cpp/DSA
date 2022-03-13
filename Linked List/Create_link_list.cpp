5a74128a729ac5671f238518fae77f92bd4ccdc8
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-13 17:24:35 +0600

    "Linked List/Create_link_list.cpp":

--- Linked List/Create_link_list.cpp
+++ Linked List/Create_link_list.cpp
@@ -1,7 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
 
+class Node
+{
+public:
+    int value;
+    Node *next;
+};
 
+int main()
+{
+
+    Node *head;
+    Node *one;
+    Node *two;
+    Node *three;
+
+    one = new Node();
+    two = new Node();       
+    three = new Node();
+
+    one->value = 1;
+    two->value = 2;
+    three->value = 3;
+
+    head = one;
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    while (head != NULL)
+    {
+        cout << head->value << " ";
+        head = head->next;
+    }
     return 0;
 }

3f75de2260c82e295944ee86b20a27195b556408
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-13 23:08:53 +0600

    "Linked List/Create_link_list.cpp":

--- Linked List/Create_link_list.cpp
+++ Linked List/Create_link_list.cpp
@@ -7,8 +7,28 @@ public:
     Node *next;
 };
 
-void push(Node *node){
+Node *deleteNode(Node *head, Node *node){
+    if(head == node){
+        head = node->next;
+        free(node);
+        return head;
+    }
 
+    Node *curr_node = head;
+    while (curr_node != NULL){
+        if(curr_node->next == node){
+            break;
+        }
+        curr_node = node->next;
+    }
+    if(curr_node == NULL){
+        return head;
+    }
+
+    curr_node->next = node->next;
+    free(node);
+
+    return head;
 }
 
 int main(){
@@ -16,19 +36,33 @@ int main(){
     Node *one;
     Node *two;
     Node *three;
+    Node *four;
+    Node *five;
+    Node *six;
 
     one = new Node();
     two = new Node();       
     three = new Node();
+    four = new Node();
+    five = new Node();
+    six = new Node();
 
     one->value = 1;
     two->value = 2;
     three->value = 3;
+    four->value = 4;
+    five->value = 5;
+    six->value = 6;
 
     head = one;
     one->next = two;
     two->next = three;
-    three->next = NULL;
+    three->next = four;
+    four->next = five;
+    five->next = six;
+    six->next = NULL;
+
+    head = deleteNode(head, two);
 
     while (head != NULL){
         cout << head->value << " ";

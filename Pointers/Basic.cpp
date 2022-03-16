d98879dad7621b94269561067aa0f4d5a07d672d
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 00:24:21 +0600

    0

--- Pointers/Basic.cpp
+++ Pointers/Basic.cpp
@@ -1,2 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+int main(){
+    int a; 
+    int *aPtr; /* hold address of variable */
+    aPtr = &a; /* &a give the address of variable and that variable adress will store in the aPtr */ 
+}

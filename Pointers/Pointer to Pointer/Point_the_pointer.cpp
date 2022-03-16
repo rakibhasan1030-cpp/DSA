1fbef36a671d121fb77a05c6edd540df39831a12
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 01:21:09 +0600

    "Pointers/Pointer to Pointer/Point_the_pointer.cpp":

--- Pointers/Pointer to Pointer/Point_the_pointer.cpp
+++ Pointers/Pointer to Pointer/Point_the_pointer.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
+    int a = 100;
+    int *p = &a;
+    cout << "Address of 'a' : " << p << endl;
+
+    int **q = &p; /* here, q is pointing to p, so q contain the address of p, and p is contain a's address */
 
     return 0;
 }

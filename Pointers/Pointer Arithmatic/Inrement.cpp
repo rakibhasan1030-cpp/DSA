a81d91ca13c8714300b2e8d055090acd5b68287c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 00:47:06 +0600

    "Pointers/Pointer Arithmatic/Inrement.cpp": create

---
+++ Pointers/Pointer Arithmatic/Inrement.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 100;
+
+    int *aPtr = &a;
+    cout << "Address of 'a' : " << aPtr << endl;
+    aPtr++;
+    cout << "Address of 'a' : " << aPtr << endl; /* this will increament the address+4, because we know, int allcate 4byte in memory */
+    return 0;
+}

9be227997e57ba6eb67b9a70e84b3cc2c191efd0
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 02:11:01 +0600

    "Linked List/Reverse_linked_list_iterative_way.cpp":

--- Linked List/Reverse_linked_list_iterative_way.cpp
+++ Linked List/Reverse_linked_list_iterative_way.cpp
@@ -12,11 +12,36 @@ class node{
     }
 };
 
-void insertAtTail(node **head, int val){
-    node n = new node(val);
+void insertAtTail(node* &head, int val){
+    node *n = new node(val);
 
+    if (head == NULL){
+        head = n;
+        return; 
+    }
+    node *temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node* head){
+    node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
 }
 
 int main(){
-    
+    node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    display(head);
 }

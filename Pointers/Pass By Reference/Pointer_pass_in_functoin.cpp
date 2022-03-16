28ff581e8421b05e8b8366bdb98ecd11dd022b31
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 01:46:21 +0600

    "Pointers/Pass By Reference/Pointer_pass_in_functoin.cpp": create

---
+++ Pointers/Pass By Reference/Pointer_pass_in_functoin.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(){
+    int a = 10, b = 20;
+    cout << "Before SWAP, A : " << a << "   |   " << "B : "<< b << endl;
+    int *aPtr, *bPtr;
+    aPtr = &a;
+    bPtr = &b;
+    swap(aPtr, bPtr);
+    cout << "After SWAP, A : " << a << "   |   "  << "B : "<< b << endl;
+    return 0;
+}

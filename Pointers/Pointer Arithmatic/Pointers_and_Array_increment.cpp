cbf839bb6a096871781268a3660ac7bb26dd5bbb
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 01:07:51 +0600

    "Pointers/Pointer Arithmatic/Pointers_and_Array_increment.cpp":

--- Pointers/Pointer Arithmatic/Pointers_and_Array_increment.cpp
+++ Pointers/Pointer Arithmatic/Pointers_and_Array_increment.cpp
@@ -12,13 +12,27 @@ void exampleOne()
     cout << "ARRAY : ";
     for(int i = 0; i < 5; i++){
         cout << *ptr << "   ";
-        ptr++; /* this will increament the address+4 (per int = 4byte), which will be the next value's address */
+        ptr++; /* this will increament the address+4 (per int = 4byte), 
+                  which will be the next value's address */
+    }
+    cout << endl;
+}
+
+void exampleTwo(){
+    cout << endl;
+    cout << "---------- EXAMPLE TWO ----------" << endl;
+    int arr[] = {10, 20, 30, 40, 50};
+    cout << "ARRAY : ";
+    for(int i = 0; i < 5; i++){
+        cout << *(arr+i) << "   "; /* here, at first, the pointer point to the first element of
+                                      array which is (arr + 0), after increasing i value the 
+                                      pointer will point respectively 1,2,3,4....ith position value */
     }
     cout << endl;
 }
 
 int main()
 {
-    exampleOne();
+    exampleTwo();
     return 0;
 }

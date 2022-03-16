79801ef97d81b43f60c29689d2bc72656550bac1
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 01:28:39 +0600

    "Pointers/Pointer to Pointer/Point_the_pointer.cpp":

--- Pointers/Pointer to Pointer/Point_the_pointer.cpp
+++ Pointers/Pointer to Pointer/Point_the_pointer.cpp
@@ -6,7 +6,15 @@ int main()
     int *p = &a;
     cout << "Address of 'a' : " << p << endl;
 
-    int **q = &p; /* here, q is pointing to p, so q contain the address of p, and p is contain a's address */
+    int **q = &p;                                               /* here, q is pointing to p, so q contain the address of p, and p is contain a's address */
+    cout << "One time de-reference of q (*q) : " << *q << endl; /* here, we de-reference of q (*q) for one time only,
+                                                                   that means it'll give me the value of p which is address
+                                                                   of a */
 
+    cout << "Two time de-reference of q (**q) : " << **q << endl; /* here, we de-reference of q (*q) for two time,
+                                                                   that means it'll give me the value of a */
+
+    **q = 200;
+    cout << "After update **q, new a's value is : "  << a << endl;                                                                
     return 0;
 }

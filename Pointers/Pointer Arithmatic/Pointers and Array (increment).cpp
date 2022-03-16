ab8e0cef1e476621b9e7db4d91242deff101f6bc
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 00:51:17 +0600

    "Pointers/Pointer Arithmatic/Pointers and Array (increment).cpp": create

---
+++ Pointers/Pointer Arithmatic/Pointers and Array (increment).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void exampleOne()
+{
+    int arr[] = {10, 20, 30, 40, 50};
+    int *ptr = arr;
+    cout << ptr << endl;
+}
+
+int main()
+{
+    exampleOne();
+    return 0;
+}

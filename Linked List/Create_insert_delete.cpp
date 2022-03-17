c3204d5c0a4f6822bd7abfcacb6c1b840b9cfda3
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-17 14:45:15 +0600

    "Linked List/Create_insert_delete.cpp": create

---
+++ Linked List/Create_insert_delete.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void insertAtBegining(node* &head, int val){
+    node *n = new node(val);
+
+    if(head == NULL){
+        head = n;
+        return;
+    }
+
+    node* temp = head;
+    head = n;
+    n->next = temp;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+
+    cout<< endl;
+    cout<< endl;
+    display(head);
+
+    insertAtBegining(head, 0);
+
+    cout<< endl;
+    cout<< endl;
+    display(head);
+}

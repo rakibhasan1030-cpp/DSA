7f6aebcf354494bb924c4f2a396c0f23a50e5499
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-18 18:52:25 +0600

    "Pointers/Pointer Arithmatic/Inrement.cpp":

--- Pointers/Pointer Arithmatic/Inrement.cpp
+++ Pointers/Pointer Arithmatic/Inrement.cpp
@@ -8,5 +8,6 @@ int main()
     cout << "Address of 'a' : " << aPtr << endl;
     aPtr++; /* this will increament the address+4, because we know, int allcate 4byte in memory */
     cout << "Address of 'a' (after inrement) : " << aPtr << endl;
+    
     return 0;
 }

2bfd352f5ad9c3b1be5962054be87f3814c33310
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-21 00:44:02 +0600

    "Linked List/Reverse_K_Nodes.cpp":

--- Linked List/Reverse_K_Nodes.cpp
+++ Linked List/Reverse_K_Nodes.cpp
@@ -1,5 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+}
+
+void reverseK(node *head, int k){
     
+}
+
+int main(){
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
+
+    cout << endl;
+    cout << endl;
+    cout << "Insert at tail : ";
+    display(head);
+
+    cout << endl;
+    cout << endl;
+
 }

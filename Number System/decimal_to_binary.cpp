7d5bf5a0dd37d98abdc8b70ab9a399d7ce9732cf
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-01 23:56:12 +0600

    "Number System/decimal_to_binary.cpp": solved

--- Number System/decimal_to_binary.cpp
+++ Number System/decimal_to_binary.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    cout << "Input : ";
+    cin>>n;
+    int b[32];
+    int i = 0;
+    while (n > 0)
+    {
+        b[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    // printing binary array in reverse order
+    for (int j = i - 1; j >= 0; j--)
+        cout << b[j];
     return 0;
 }

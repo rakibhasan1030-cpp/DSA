649e97c93ea7bd8f15e5493eb99f2c502949e671
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-08 00:16:27 +0600

    "Number System/binery_to_dacimal.cpp":

--- Number System/binery_to_dacimal.cpp
+++ Number System/binery_to_dacimal.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     string a;
     int ans = 0;
+    
     cout << "Input : ";
     cin>>a;
     for(int i = 0; i < a.size(); i++){
@@ -10,5 +11,6 @@ int main(){
         x == 1 ? ans += pow(2, i) : 0;
     }
     cout << "Decimal number of given decimal number(" << a << ") is : " << ans << endl; 
+
     return 0;
 }

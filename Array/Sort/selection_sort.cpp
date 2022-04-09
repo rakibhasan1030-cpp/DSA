ef38aea0320110f9cfa3187f811ffb3b8da4c91a
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-09 15:48:03 +0600

    Array/Sort/selection_sort: create

---
+++ Array/Sort/selection_sort.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter array size : ";
+    cin>>n;
+    int arr[n];
+    cout<<endl;
+    cout<<"Enter value : " << endl;
+    for (size_t i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    cout<<"Your array is : ";
+    for (size_t i = 0; i < n; i++)
+    {
+        cout<< arr[i] << "   ";
+    }
+}

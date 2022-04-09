077167cf3080ac18cf170ad563ec9a59b1b30ded
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-09 14:55:56 +0600

    Array/Search/linear_search: create

---
+++ Array/Search/linear_search.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int linearSearch(){
+
+    return -1;
+}
+
+int main(){
+    int n;
+    cout<<"Enter array size : ";
+    cin>>n;
+    int arr[n];
+    cout<<endl;
+    cout<<"Enter value : " << endl;
+    for (size_t i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+
+    cout<<"Your array is : ";
+    for (size_t i = 0; i < n; i++)
+    {
+        cout<< arr[i] << "   ";
+
+    }
+
+}

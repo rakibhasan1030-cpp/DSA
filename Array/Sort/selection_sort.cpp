047094d2d3b435ec0c6e4ca7d49135d1a1ebf1fb
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-09 16:12:46 +0600

    Array/Sort/selection_sort:

--- Array/Sort/selection_sort.cpp
+++ Array/Sort/selection_sort.cpp
@@ -1,5 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+/* Find the minimum element in a unsorted array and swap it with the element at begining */
+void selectionSort(int arr[], int n){
+    for (size_t i = 0; i < n-1; i++)
+    {
+        for (size_t j = i+1; j < n; j++)
+        {
+            if(arr[j] < arr[i]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    
+    cout<<endl;
+    cout<<"Your sorted array is : ";
+    for (size_t i = 0; i < n; i++)
+    {
+        cout<< arr[i] << "   ";
+    }
+    
+}
+
 int main(){
     int n;
     cout<<"Enter array size : ";
@@ -17,4 +41,5 @@ int main(){
     {
         cout<< arr[i] << "   ";
     }
+    selectionSort(arr, n);
 }

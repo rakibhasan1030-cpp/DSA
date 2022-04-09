85193af39f8350d40399cc8227dccb6d677e1d06
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-09 15:09:45 +0600

    Array/Search/linear_search:

--- Array/Search/linear_search.cpp
+++ Array/Search/linear_search.cpp
@@ -1,8 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int linearSearch(){
-
+int linearSearch(int arr[], int size, int key){
+    for (size_t i = 0; i < size; i++)
+    {
+        if(key == arr[i]) return i;
+    }
+    
     return -1;
 }
 
@@ -18,12 +22,14 @@ int main(){
         cin>>arr[i];
     }
     
-
     cout<<"Your array is : ";
     for (size_t i = 0; i < n; i++)
     {
         cout<< arr[i] << "   ";
-
     }
-
+    cout<<endl;
+    int s;
+    cout<<"Enter your search value : ";
+    cin>>s;
+    cout<< "Found your value at position : " << linearSearch(arr, n, s);
 }

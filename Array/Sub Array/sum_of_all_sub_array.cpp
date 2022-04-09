4d551dc9e6e38f6bd1a9ee8efc6eb9ae52bf8874
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-10 01:01:14 +0600

    "Array/Sub Array/sum_of_all_sub_array.cpp":

--- Array/Sub Array/sum_of_all_sub_array.cpp
+++ Array/Sub Array/sum_of_all_sub_array.cpp
@@ -1,12 +1,27 @@
+
+/* NUMBER OF SUBARRAY OF AN ARRAY WITH N ELEMENTS = n * (n+1) /2  */
+
 #include<bits/stdc++.h>
 using namespace std;
+
+void sumOfAllSubArray(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+        for(int j = i; j < n; j++){
+            sum += arr[j];
+            cout << sum << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     int n;
     cout<<"Enter array size : ";
     cin>>n;
     int arr[n];
     cout<<endl;
-    cout<<"Enter value : (array must be sorted!)" << endl;
+    cout<<"Enter value : " << endl;
     for (size_t i = 0; i < n; i++)
     {
         cin>>arr[i];
@@ -17,4 +32,7 @@ int main(){
     {
         cout<< arr[i] << "   ";
     }
+    cout<<endl;
+    cout<<endl;
+    sumOfAllSubArray(arr, n);
 }

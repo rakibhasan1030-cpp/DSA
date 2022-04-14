9ce97d1e4183cd9bb5578b8474f73b8e92cf7c29
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-15 01:16:15 +0600

    "Array/2D Array/searching_matrix.cpp":

--- Array/2D Array/searching_matrix.cpp
+++ Array/2D Array/searching_matrix.cpp
@@ -3,13 +3,23 @@ using namespace std;
 const int M=3, N=3;
 
 void linearSearch(int arr[M][N], int key){
-    
+    bool flag = false;
+    for (size_t i = 0; i < M; i++)
+    {
+        for (size_t j = 0; j < N; j++)
+        {
+            if(arr[M][N]  == key) flag = true;
+        }
+    }
+    if(flag) cout << "Found!";
+    else cout << "Not Found!";
 }
 
 int main(){
 
     int arr[M][N];
-    
+    cout<< "----- Enter value for 3X3 MATRIX -----" <<endl;
+    cout << endl;
     for (size_t i = 0; i < M; i++)
     {
         for (size_t j = 0; j < N; j++)
@@ -38,4 +48,4 @@ int main(){
     linearSearch(arr, key);
 
     return 0;
-}
+} 

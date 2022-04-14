cb49d27ab0168306c4e6fa5ea83dcae60f651c2c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-15 00:46:29 +0600

    "Array/2D Array/searching_matrix.cpp":

--- Array/2D Array/searching_matrix.cpp
+++ Array/2D Array/searching_matrix.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int main(){
     int row, col;
+    cout<< "Input ROW and COLUMN number : ";
     cin>>row>>col;
     int arr[row][col];
     
@@ -10,10 +11,22 @@ int main(){
     {
         for (size_t j = 0; j < col; j++)
         {
+            cout<< "Enter [" << i << j << "]-th element : ";
             cin>>arr[i][j];
         }
     }
     
+    cout << endl;
+    cout << " Your matrix is : " << endl;
+
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            cout<<arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }

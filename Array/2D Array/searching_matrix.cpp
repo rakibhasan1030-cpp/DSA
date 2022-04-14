ab202fb67dcddeb9563c852d282dc0f01a23e11e
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-15 00:42:42 +0600

    "Array/2D Array/searching_matrix.cpp":

--- Array/2D Array/searching_matrix.cpp
+++ Array/2D Array/searching_matrix.cpp
@@ -2,6 +2,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int row, col;
+    cin>>row>>col;
+    int arr[row][col];
+    
+    for (size_t i = 0; i < row; i++)
+    {
+        for (size_t j = 0; j < col; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    
 
     return 0;
 }

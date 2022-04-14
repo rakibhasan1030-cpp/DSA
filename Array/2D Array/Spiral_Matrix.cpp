6a3802848881057f33329cb2376a41b9a52b1f5e
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-15 02:31:20 +0600

    "Array/2D Array/Spiral_Matrix.cpp":

---
+++ Array/2D Array/Spiral_Matrix.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N, M;
+    cout<< "Enter ROW for the MATRIX : ";
+    cin>>N;
+    
+    cout << endl;
+    cout<< "Enter COLUMN for the MATRIX : ";
+    cin>>M;
+
+    int arr[N][M];
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < M; j++)
+        {
+            cout<< "Enter [" << i << j << "]-th element : ";
+            cin>>arr[i][j];
+        }
+    }
+    
+    cout << endl;
+    cout << "Your matrix is : " << endl;
+
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < M; j++)
+        {
+            cout<<arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}

61ad3a14168422da2a7bf1705532b92434430489
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-20 02:28:11 +0600

    "Bit Manipulation/Problems/Odd_Even.cpp": create

---
+++ Bit Manipulation/Problems/Odd_Even.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Input : ";
+    cin>>n;
+    if(n & 1 == 0){ /* If we mask 1 with & operation with a number, then we can find the last bit of that number,
+                       and if we get the last bit of any number then we can say, 1 = ODD and 0 = EVEN */
+        cout << n << " is a Even Number." <<endl;
+    }else {
+        cout << n << " is a ODD Number." <<endl;
+    }
+    return 0;
+}

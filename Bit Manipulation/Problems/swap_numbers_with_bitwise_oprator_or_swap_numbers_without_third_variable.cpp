89bd3e389ec81d7795f331ae07e8715c4846e936
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-20 02:59:12 +0600

    "Bit Manipulation/Problems/swap_numbers_with_bitwise_oprator_or_swap_numbers_without_third_variable.cpp":

--- Bit Manipulation/Problems/swap_numbers_with_bitwise_oprator_or_swap_numbers_without_third_variable.cpp
+++ Bit Manipulation/Problems/swap_numbers_with_bitwise_oprator_or_swap_numbers_without_third_variable.cpp
@@ -7,6 +7,15 @@ int main()
     a = (a^b);
     b = (a^b);
     a = (a^b);
+
+    //a = b^a^(b = a); 
+    /* Which is, when you xor (^) two same numbers the result is 0, it's like they cancels each other. Now using this
+    a = b ^ a ^ (a = b);
+    -> a = b ^ a ^ a [since now a = b]
+    [So now a^a = 0.]
+    -> b = a ^ 0 
+    -> b = a */
+
     cout << "After swap : " << "A = " << a << "   |   B = " << b << endl; 
     return 0;
 }

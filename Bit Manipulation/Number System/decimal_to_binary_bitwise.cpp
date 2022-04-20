70e1750065c323b1bdb652c1acc847e463bef1c4
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-20 23:46:25 +0600

    "Bit Manipulation/Number System/decimal_to_binary_bitwise.cpp": create

---
+++ Bit Manipulation/Number System/decimal_to_binary_bitwise.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+string decimalToBinary(int n){
+    string nStr;
+    while(n){
+        if (n & 1){
+            nStr+='1';
+        }else{
+            nStr+='0';
+        }
+        n >>= 1;
+    }
+    reverse(nStr.begin(), nStr.end());
+    return nStr;
+}
+
+int main(){
+    int n;
+    cout << "Enter your value(in decimal) : ";
+    cin>>n;
+    cout << "Binary of " << n << "is : " << decimalToBinary(n) << endl;
+}

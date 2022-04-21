7bdcc12fd26bd2c5d96be32c0040efa8c569746c
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-22 02:21:17 +0600

    "Bit Manipulation/Bit Masking/clear_ith_bit.cpp":

--- Bit Manipulation/Bit Masking/clear_ith_bit.cpp
+++ Bit Manipulation/Bit Masking/clear_ith_bit.cpp
@@ -1,8 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+int clearTheBit(int n, int i){
+    int mask = ~(1 << i); /* left shift one i time and invert the bits */
+    int ans = (n & mask); /* doing AND operation with mask so, that i-th bit became 0 or clear */
+    return ans; /* or, simply return (n | (~(1 << i))) [ one line solution ]*/
+}
 
-    
+int main(){
+    int n, i;
+
+    cout << "Enter your value(in decimal) : ";
+    cin>>n;
+
+    cout << "Enter your bit position : ";
+    cin>>i;
+
+    /* INPUT - n = 5 (101), i = 1
+       OUTPUT - ans = 7 (111), 1-th position bit 0 became 1, which value is 7 */
+    cout << "After clear the bit bit at " << i <<"-the position, the new value is = " << clearTheBit(n, i) << endl;
 
     return 0;
 }

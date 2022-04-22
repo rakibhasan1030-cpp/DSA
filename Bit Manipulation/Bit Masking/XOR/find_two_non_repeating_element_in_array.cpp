7ee6d7d2113110948c502161c09acd850c7b06de
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-23 00:40:14 +0600

    "Bit Manipulation/Bit Masking/XOR/find_two_non_repeating_element_in_array.cpp": create

---
+++ Bit Manipulation/Bit Masking/XOR/find_two_non_repeating_element_in_array.cpp
@@ -0,0 +1,38 @@
+/* Find the two non-repeating elements in an array of repeating elements */
+#include<bits/stdc++.h>
+using namespace std;
+
+void get2NonRepeatingNumbers(int arr[], int n){
+
+    int setBitPos, x = 0, y = 0, xorr = 0;
+
+    /* XOR all elements */
+    for (int i = 0; i < n; i++)
+    {
+        xorr ^= arr[i];
+    }
+    
+    /* Get the rightmost set bit in setBitPos, FORMULLA : n ^ ~(n-1)*/
+    setBitPos = xorr ^ ~(xorr - 1);
+
+    /* Now divide elements in two sets by
+    comparing rightmost set bit of Xor with bit
+    at same position in each element. */
+    for (int i = 0; i < n; i++)
+    {
+        if((arr[i] & setBitPos) == 1){
+            x = xorr ^ arr[i];
+        }else{
+            y = xorr ^ arr[i];
+        }
+    }
+    cout << "The non-repeating elements are " << x << " and " << y;
+}
+
+int main(){
+
+    int arr[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4, 8 };
+    int n = (sizeof(arr) / sizeof(arr[0]));
+    get2NonRepeatingNumbers(arr, n);
+    return 0;
+}

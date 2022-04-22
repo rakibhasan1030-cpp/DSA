f8afbbaafa68fe180a57155de67b389045703c64
Rakib Hasan <rakibhasan1030@gmail.com>
2022-04-22 23:13:22 +0600

    "Bit Manipulation/Bit Masking/XOR/non_repeating_element_in_array.cpp":

--- Bit Manipulation/Bit Masking/XOR/non_repeating_element_in_array.cpp
+++ Bit Manipulation/Bit Masking/XOR/non_repeating_element_in_array.cpp
@@ -1,7 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
 
+    int arr[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+    int res = 0;
+    for (int i = 0; i < (sizeof(arr) / sizeof(arr[0])); i++){
+        res = res ^ arr[i];
+    }
+    cout << "Ans : " << res << endl;
     return 0;
 }
